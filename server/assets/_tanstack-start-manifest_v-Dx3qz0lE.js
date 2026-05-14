aa576cb994ff1e2b0c5419af268263e46dbd6938
samueldevpires <samueleleefepe@gmail.com>
2026-05-14 15:34:14 -0300

    Updates

--- server/assets/_tanstack-start-manifest_v-Dx3qz0lE.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/samue/Desktop/digital-impact-agency-main/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-Dmq9Uzwc.js"] }, "/": { filePath: "C:/Users/samue/Desktop/digital-impact-agency-main/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-CoSHVTsu.js"] } }, clientEntry: "/assets/index-Dmq9Uzwc.js" });
-export {
-  tsrStartManifest
-};
--- server/index.js
+++
@@ -1,5 +0,0 @@
-import { w } from "./assets/worker-entry-ChwjHX4G.js";
-import "node:events";
-export {
-  w as default
-};
